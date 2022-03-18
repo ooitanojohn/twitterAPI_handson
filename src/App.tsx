e410b9f0b0bbb192ce9c99712a196b0dbe0ab2d7
ooitanojohn <qmkh733310@gmail.com>
2022-03-18 14:36:07 +0900

    デプロイ

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MainTable from "./Components/MainTable";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HelmetProvider>
+      <Helmet>
+        <title>iine-app-handson</title>
+      </Helmet>
+      <div className="bg-blue-50 min-h-screen">
+        <div className="container mx-auto">
+          <header className="flex justify-center items-center text-3xl h-32 mx-5">
+            いいねした画像を並べるサイト
+          </header>
+          <div className="flex justify-center">
+            <MainTable />
+          </div>
+        </div>
+      </div>
+    </HelmetProvider>
   );
 }
 
